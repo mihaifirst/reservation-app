44bbf3d1cb362b739daa56dedcf23ee5979d09a2
Sorin Vestemean <vestemeansorin91@gmail.com>
2025-01-06 14:15:25 +0200

    refactoring + calendar form modal

---
+++ frontend/src/components/calendar-form-modal/calendar-form-modal.component.jsx
@@ -0,0 +1,114 @@
+import Modal from "../modal/modal.component";
+import formatDate from "../../helpers/calendar.helpers";
+import React, {useEffect, useState} from "react";
+
+const modalDefaultFormFields = {
+    date: null,
+    startHour: 12,
+    endHour: 18,
+    hourRange: 60,
+};
+
+const CalendarFormModal = ({isOpen, onClose, selectedDate, onSubmit}) => {
+    const submitButtonLabel = 'Creeaza calendar';
+    const modalTitleLabel = selectedDate && `Creaza tabel pentru data: ${formatDate(selectedDate)}`;
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [formFields, setFormFields] = useState(
+        modalDefaultFormFields
+    );
+
+    useEffect(() => {
+        if (selectedDate) {
+            setFormFields({...formFields, date: formatDate(selectedDate)});
+        }
+    }, [selectedDate])
+
+    useEffect(() => {
+        if(isOpen) {
+            setIsModalOpen(true);
+        }
+    }, [isOpen]);
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+        onClose();
+    };
+
+    const onCloseHandler = () => {
+        closeModal();
+    }
+
+    const onSubmitHandler = () => {
+        onSubmit(formFields);
+        closeModal();
+    }
+
+    const onFormFieldValueChange = (event) => {
+        const {value, name} = event.target;
+        setFormFields({...formFields, date: selectedDate, [name]: Number(value)});
+    };
+
+    return <Modal
+        isOpen={isModalOpen}
+        onClose={onCloseHandler}
+        onSubmit={onSubmitHandler}
+        titleLabel={modalTitleLabel}
+        submitButtonLabel={submitButtonLabel}>
+        <form className="formular">
+            <div className="formDiv">
+                <label htmlFor="startHour" className="formLable">
+                    StartHour
+                </label>
+
+                <input
+                    type="number"
+                    value={formFields.startHour}
+                    name="startHour"
+                    min="6"
+                    max="12"
+                    onChange={onFormFieldValueChange}
+                    className="formInput"
+                />
+            </div>
+            <div className="formDiv">
+                <label className="formLable">EndHour</label>
+                <input
+                    type="number"
+                    value={formFields.endHour}
+                    min="18"
+                    max="24"
+                    name="endHour"
+                    onChange={onFormFieldValueChange}
+                    className="formInput"
+                />
+            </div>
+            <label className="formDiv">
+                Hour Range:
+                <div>
+                    <input
+                        type="radio"
+                        id="hour30"
+                        name="hourRange"
+                        value="30"
+                        checked={formFields.hourRange === 30}
+                        onChange={onFormFieldValueChange}
+                    />
+                    <label htmlFor="hour30">30</label>
+                </div>
+                <div>
+                    <input
+                        type="radio"
+                        id="hour60"
+                        name="hourRange"
+                        value="60"
+                        checked={formFields.hourRange === 60}
+                        onChange={onFormFieldValueChange}
+                    />
+                    <label htmlFor="hour30">60</label>
+                </div>
+            </label>
+        </form>
+    </Modal>
+}
+
+export default CalendarFormModal;
--- frontend/src/components/date-picker/date-picker.component.jsx
+++ frontend/src/components/date-picker/date-picker.component.jsx
@@ -1,35 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect, Fragment} from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import style from "./date-picker.module.css";
 
-const DatePickerComponent = ({ onChangeDate }) => {
-  const [selectedDate, setSelectedDate] = useState(new Date());
+const DatePickerComponent = ({onChangeDate}) => {
+    const [selectedDate, setSelectedDate] = useState(new Date());
 
-  const dateChange = (event) => {
-    setSelectedDate(event);
-    onChangeDate(event);
-  };
+    const dateChange = (event) => {
+        setSelectedDate(event);
+        onChangeDate(event);
+    };
 
-  useEffect(() => {
-    onChangeDate(selectedDate);
-  }, [selectedDate, onChangeDate]);
+    useEffect(() => {
+        onChangeDate(selectedDate);
+    }, [selectedDate, onChangeDate]);
 
-  return (
-    <div>
-      <h1>Select a date</h1>
-      {selectedDate && (
-        <p className={style.datePicker}>
-          Selected date: {selectedDate.toLocaleDateString()}
-        </p>
-      )}
-      <DatePicker
-        selected={selectedDate}
-        onChange={dateChange}
-        dateFormat="MM/dd/yyyy"
-      />
-    </div>
-  );
+    return (
+        <div>
+            {selectedDate && <Fragment>
+                <h3 className={style.datePicker}>Data selectata:</h3>
+                <h3>{selectedDate.toLocaleDateString()}</h3>
+            </Fragment>}
+
+            <DatePicker
+                selected={selectedDate}
+                onChange={dateChange}
+                dateFormat="dd/MM/yyyy"
+            />
+        </div>
+    );
 };
 
 export default DatePickerComponent;
